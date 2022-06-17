9f680a87b202d107c1fb4bf0d21e958cff5cb662
teeradon43 <teeradonch43@gmail.com>
2022-06-17 17:28:14 +0700

    DI/DO

--- src/uc300_encoder.js
+++ src/uc300_encoder.js
@@ -2,40 +2,102 @@
 // console.log('Thu Apr 14 2022 09:01:30 GMT+0700 (เวลาอินโดจีน)')
 // let deserialized = new Date('Thu Apr 14 2022 09:01:30 GMT+0700 (เวลาอินโดจีน)')
 // console.log(Number(deserialized))
+
+/***********************************/
+
+/******** Data Transform ********/
 function reverseHex(hexString) {
-  var chunks = [];
-  for (var i = 0, charsLength = hexString.length; i < charsLength; i += 2) {
-    chunks.push(hexString.substring(i, i + 2));
+  let str = [];
+  for (let i = 0, charsLength = hexString.length; i < charsLength; i += 2) {
+    str.push(hexString.substring(i, i + 2));
   }
-  chunks.reverse();
-  return chunks.join("");
+  str.reverse();
+  return str.join("");
 }
 
-function getPacketLengthHex(value) {
+function NumToHexString(value, numberOfByte = 1) {
   let hexString = value.toString(16);
-  hexString = hexString.padStart(4, "0");
+  hexString = hexString.padStart(numberOfByte * 2, "0");
+  return hexString;
+}
+/***********************************/
+
+/******** Get Hex from Data ********/
+function getPacketLengthHex(value) {
+  let hexString = NumToHexString(value, 2);
   return reverseHex(hexString);
 }
 
 function getPacketVersionHex(value) {
-  let hexString = value.toString(16);
-  return hexString.padStart(2, "0");
+  return NumToHexString(value, 1);
 }
 
 function getTimestampHex(dateString) {
   let dateNum = Number(new Date(dateString)) / 1000;
-  let hexString = dateNum.toString(16);
-  hexString = hexString.padStart(4, "0");
+  let hexString = NumToHexString(dateNum, 4);
   return reverseHex(hexString);
 }
 
-console.log(
-  getTimestampHex("Thu Apr 14 2022 09:01:30 GMT+0700 (เวลาอินโดจีน)")
-);
+function getSignalStrengthHex(value) {
+  return NumToHexString(value, 1);
+}
+
+function getDigitalOutputTogglesHex(digitalOutputToggles) {
+  let result = 0;
+  for (const toggles of digitalOutputToggles) {
+    if (toggles.name === "DO1") result += toggles.toggle;
+    else if (toggles.name === "DO2") result += toggles.toggle * 2;
+  }
+  return NumToHexString(result, 1);
+}
+
+function getDigitalOutputStatusesHex(digitalOutputStatuses) {
+  let result = 0;
+  for (const statuses of digitalOutputStatuses) {
+    if (statuses.name === "DO1") result += statuses.status;
+    else if (statuses.name === "DO2") result += statuses.status * 2;
+  }
+  return NumToHexString(result, 1);
+}
+
+function getDigitalInputTogglesHex(digitalInputToggles) {
+  let result = 0;
+  for (const index in digitalInputToggles) {
+    result += digitalInputToggles[index].toggle << (2 * index);
+  }
+  return NumToHexString(result, 1);
+}
+
+function getDigitalInputStatusesHex(digitalInputStatuses) {
+  let result = 0;
+  for (const index in digitalInputStatuses) {
+    result += digitalInputStatuses[index].status << index;
+  }
+  return NumToHexString(result, 1);
+}
+
+function getDigitalInputCountersHex(digitalInputCounter) {
+  let result = "";
+  let hexString = "";
+  for (const { counter } of digitalInputCounter) {
+    if (counter != null) {
+      hexString = NumToHexString(counter, 4);
+      result += reverseHex(hexString);
+    }
+  }
+  return result;
+}
+/***********************************/
 
 module.exports = {
   reverseHex,
   getPacketLengthHex,
   getPacketVersionHex,
   getTimestampHex,
+  getSignalStrengthHex,
+  getDigitalOutputTogglesHex,
+  getDigitalOutputStatusesHex,
+  getDigitalInputTogglesHex,
+  getDigitalInputStatusesHex,
+  getDigitalInputCountersHex,
 };
--- src/uc300_encoder.test.js
+++ src/uc300_encoder.test.js
@@ -3,6 +3,12 @@ const {
   getPacketLengthHex,
   getPacketVersionHex,
   getTimestampHex,
+  getSignalStrengthHex,
+  getDigitalOutputTogglesHex,
+  getDigitalOutputStatusesHex,
+  getDigitalInputTogglesHex,
+  getDigitalInputStatusesHex,
+  getDigitalInputCountersHex,
 } = require("./uc300_encoder");
 
 const MESSAGE = {
@@ -48,7 +54,7 @@ test("getPacketVersionHex", () => {
   expect(getPacketVersionHex(255)).toBe("ff");
 });
 
-test("", () => {
+test("getTimestampHex", () => {
   expect(
     getTimestampHex("Thu Apr 14 2022 09:01:30 GMT+0700 (เวลาอินโดจีน)")
   ).toBe("7a805762");
@@ -59,6 +65,139 @@ test("", () => {
   expect(
     getTimestampHex("Fri May 27 2022 15:17:46 GMT+0700 (เวลาอินโดจีน)")
   ).toBe("2a899062");
+});
+
+test("getSignalStrengthHex", () => {
+  expect(getSignalStrengthHex(0)).toBe("00");
+  expect(getSignalStrengthHex(1)).toBe("01");
+  expect(getSignalStrengthHex(10)).toBe("0a");
+  expect(getSignalStrengthHex(16)).toBe("10");
+  expect(getSignalStrengthHex(25)).toBe("19");
+  expect(getSignalStrengthHex(255)).toBe("ff");
+});
+
+const DIGITAL_OUTPUT_TOGGLES_TEMPLATE = [
+  { name: "DO1", toggle: null },
+  { name: "DO2", toggle: null },
+];
+
+test("getDigitalOutputTogglesHex", () => {
+  let DOToggles = [...DIGITAL_OUTPUT_TOGGLES_TEMPLATE];
+  DOToggles[0].toggle = 0;
+  DOToggles[1].toggle = 0;
+  expect(getDigitalOutputTogglesHex(DOToggles)).toBe("00");
+  DOToggles[0].toggle = 1;
+  expect(getDigitalOutputTogglesHex(DOToggles)).toBe("01");
+  DOToggles[1].toggle = 1;
+  expect(getDigitalOutputTogglesHex(DOToggles)).toBe("03");
+  DOToggles[0].toggle = 0;
+  expect(getDigitalOutputTogglesHex(DOToggles)).toBe("02");
+});
+
+const DIGITAL_OUTPUT_STATUSES_TEMPLATE = [
+  { name: "DO1", status: null },
+  { name: "DO2", status: null },
+];
+
+test("getDigitalOutputStatusesHex", () => {
+  let DOStatuses = [...DIGITAL_OUTPUT_STATUSES_TEMPLATE];
+  DOStatuses[0].status = 0;
+  DOStatuses[1].status = 0;
+  expect(getDigitalOutputStatusesHex(DOStatuses)).toBe("00");
+  DOStatuses[0].status = 1;
+  expect(getDigitalOutputStatusesHex(DOStatuses)).toBe("01");
+  DOStatuses[1].status = 1;
+  expect(getDigitalOutputStatusesHex(DOStatuses)).toBe("03");
+  DOStatuses[0].status = 0;
+  expect(getDigitalOutputStatusesHex(DOStatuses)).toBe("02");
+});
+
+const DIGITAL_INPUT_TOGGLES_TEMPLATE = [
+  { name: "DI1", toggle: null },
+  { name: "DI2", toggle: null },
+  { name: "DI3", toggle: null },
+  { name: "DI4", toggle: null },
+];
+
+test("getDigitalInputTogglesHex", () => {
+  let DIToggles = [...DIGITAL_INPUT_TOGGLES_TEMPLATE];
+  DIToggles[0].toggle = 0;
+  DIToggles[1].toggle = 0;
+  DIToggles[2].toggle = 0;
+  DIToggles[3].toggle = 0;
+  expect(getDigitalInputTogglesHex(DIToggles)).toBe("00");
+  DIToggles[0].toggle = 1;
+  DIToggles[1].toggle = 1;
+  DIToggles[2].toggle = 1;
+  DIToggles[3].toggle = 1;
+  expect(getDigitalInputTogglesHex(DIToggles)).toBe("55");
+  DIToggles[0].toggle = 2;
+  DIToggles[1].toggle = 2;
+  DIToggles[2].toggle = 2;
+  DIToggles[3].toggle = 2;
+  expect(getDigitalInputTogglesHex(DIToggles)).toBe("aa");
+  DIToggles[0].toggle = 3;
+  DIToggles[1].toggle = 3;
+  DIToggles[2].toggle = 3;
+  DIToggles[3].toggle = 3;
+  expect(getDigitalInputTogglesHex(DIToggles)).toBe("ff");
+  DIToggles[0].toggle = 0;
+  DIToggles[1].toggle = 1;
+  DIToggles[2].toggle = 2;
+  DIToggles[3].toggle = 3;
+  expect(getDigitalInputTogglesHex(DIToggles)).toBe("e4");
+});
+
+const DIGITAL_INPUT_STATUSES_TEMPLATE = [
+  { name: "DI1", status: null },
+  { name: "DI2", status: null },
+  { name: "DI3", status: null },
+  { name: "DI4", status: null },
+];
+
+test("getDigitalInputStatusesHex", () => {
+  let DIStatuses = [...DIGITAL_INPUT_STATUSES_TEMPLATE];
+  DIStatuses[0].status = 0;
+  DIStatuses[1].status = 0;
+  DIStatuses[2].status = 0;
+  DIStatuses[3].status = 0;
+  expect(getDigitalInputStatusesHex(DIStatuses)).toBe("00");
+  DIStatuses[0].status = 0;
+  DIStatuses[1].status = 1;
+  DIStatuses[2].status = 1;
+  DIStatuses[3].status = 0;
+  expect(getDigitalInputStatusesHex(DIStatuses)).toBe("06");
+  DIStatuses[0].status = 1;
+  DIStatuses[1].status = 0;
+  DIStatuses[2].status = 0;
+  DIStatuses[3].status = 1;
+  expect(getDigitalInputStatusesHex(DIStatuses)).toBe("09");
+  DIStatuses[0].status = 1;
+  DIStatuses[1].status = 1;
+  DIStatuses[2].status = 1;
+  DIStatuses[3].status = 1;
+  expect(getDigitalInputStatusesHex(DIStatuses)).toBe("0f");
+});
+
+const DIGITAL_INPUT_COUNTERS_TEMPLATE = [
+  { name: "DI1", counter: null },
+  { name: "DI2", counter: null },
+  { name: "DI3", counter: null },
+  { name: "DI4", counter: null },
+];
+
+test("getDigitalInputCountersHex", () => {
+  let DICounters = [...DIGITAL_INPUT_COUNTERS_TEMPLATE];
+  expect(getDigitalInputCountersHex(DICounters)).toBe("");
+  DICounters[1].counter = 0;
+  DICounters[3].counter = 21;
+  expect(getDigitalInputCountersHex(DICounters)).toBe("0000000015000000");
+  DICounters[0].counter = 0;
+  DICounters[2].counter = 0;
+  DICounters[3].counter = 0;
+  expect(getDigitalInputCountersHex(DICounters)).toBe(
+    "00000000000000000000000000000000"
+  );
 });
 
 // test("encode case 1", () => {
